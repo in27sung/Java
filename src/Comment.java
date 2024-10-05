4a33b4f9c410d6f54e2255177da522cac4590e4c
Insung <110476794+in27sung@users.noreply.github.com>
2024-10-05 21:41:53 +0900

    Learn basic java

---
+++ src/Comment.java
@@ -0,0 +1,30 @@
+public class Comment {
+
+    public static void main(String[] args) {
+        //hello java1을 출력합니다.
+        System.out.println("Hello, java1");
+//        System.out.println("Hello, java2");
+
+        /*
+        System.out.println("Hello, java3");
+        System.out.println("Hello, java4");
+        */
+    }
+}
+
+
+/*
+* 자바 표준 스펙
+* 자바 컴파일러
+* 자바 실행 라이브러리
+* 자바 가상 머신(JVM)
+*
+* https://whichjdk.com JDK 확인 웹사이트
+*
+* 컴파일과 실행
+* Hello.java -> 컴파일러 -> Hello.class file 생성 -> JVM 실행 -> 프로그램 작동
+*
+*자바 프로그램은 자바가 설치된 모든 OS에서 실행할 수 있다.
+* 
+*
+* */
---
+++ src/HelloJava.java
@@ -0,0 +1,10 @@
+import java.time.Clock;
+
+public class HelloJava {
+
+    public static void main(String[] args) { //main method
+        System.out.println("Hello, java");
+    }// main()
+}// class HelloJava
+
+// tab = space 4 times
